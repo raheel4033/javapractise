3d899d59c4377236ce61d837c4c44b3ec75bc4a7
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-07 19:31:48 +0500

    multilevel interface
    multilevel inheritence can be acheived by using the interface and extend that with the interfaces and then finally calling them in the main function.

--- interfac.java
+++ interfac.java
@@ -1,6 +1,11 @@
 public class interfac {
-
-    
+        public static void main(String[] args)
+        {
+            same s = new same();
+            s.na();
+            s.nna();
+            s.nnna();
+        }
 
 
 }
@@ -8,17 +13,17 @@ class same implements name
 {
     @Override
     public void na() {
-
+    System.out.println("Hello na");
     }
 
     @Override
     public void nna() {
-
+    System.out.println("Hello nna");
     }
 
     @Override
     public void nnna() {
-
+    System.out.println("Hello nnna");
     }
 }
 interface name extends name1,name2
