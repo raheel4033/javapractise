39b77b9de93c2f40f4684d7597145d6b4f016f63
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-07 19:24:13 +0500

    interface and override
    Learn how to implement interface and extends different interfaces and implements to the class where it is used

---
+++ interfac.java
@@ -0,0 +1,36 @@
+public class interfac {
+
+    
+
+
+}
+class same implements name
+{
+    @Override
+    public void na() {
+
+    }
+
+    @Override
+    public void nna() {
+
+    }
+
+    @Override
+    public void nnna() {
+
+    }
+}
+interface name extends name1,name2
+{
+    void na();
+}
+interface name1
+{
+    void nna();
+}
+interface name2
+{
+    void nnna();
+
+}
