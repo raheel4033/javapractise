57637536aa2d3c5e7d8f207ad84b06b3f7df3748
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-08 22:45:04 +0500

    Threading in java
    Thread creation in java and running demonstration on method level

--- packconcept.java
+++ packconcept.java
@@ -2,7 +2,9 @@ class packconcept {
     public static void main(String[] args)
     {
         myclass myc = new myclass();
-        myc.run();
+        myclass2 myc2 = new myclass2();
+        myc.start();
+        myc2.start();
     }
 
 }
@@ -14,5 +16,15 @@ class myclass extends Thread implements Runnable
             System.out.println("Starting from Thread:"+i);
         }
         System.out.println("Exiting from Thread");
+    }
+}
+class myclass2 extends Thread implements Runnable
+{
+    public void run() {
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println("Starting from Thread 2:"+i);
+        }
+        System.out.println("Exiting from Thread 2");
     }
 }
