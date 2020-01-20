a9c788ed85eb6196b8e9c3324242b3a5dab55129
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-20 13:44:17 +0500

    starpattern
    star pattern has been implemented with the for loop.

---
+++ pattern.java
@@ -0,0 +1,18 @@
+public class DataStructuresInJava {
+    public static void main(String[] args)
+    {
+    for(int i=0;i<6;i++)
+    {
+        for (int j=0;j<i;j++)
+        {
+            System.out.print(j);
+        }
+        for (int k=6;k>i;k--)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+    }
+}
